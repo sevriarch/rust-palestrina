376999afcdfefc2fc7d730503747205959aa76a2
S <sevriarch@gmail.com>
2024-12-11 01:04:33 -0500

    primepi import

--- src/imports.rs
+++ src/imports.rs
@@ -30,6 +30,21 @@ pub fn primes(n: usize) -> Vec<i64> {
     ret
 }
 
+pub fn primepi(n: usize) -> Vec<i64> {
+    let mut ret = Vec::with_capacity(n);
+    let mut count = 0;
+
+    for i in 0..n {
+        if is_prime(i as i64) {
+            count += 1;
+        }
+
+        ret.push(count);
+    }
+
+    ret
+}
+
 pub fn infinity(n: usize) -> Vec<i64> {
     let mut ret = Vec::with_capacity(n);
 
@@ -58,6 +73,13 @@ mod tests {
         assert_eq!(primes(0), vec![]);
         assert_eq!(primes(4), vec![2,3,5,7]);
         assert_eq!(primes(32), vec![2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131]);
+    }
+
+    #[test]
+    fn test_primepi() {
+        assert_eq!(primepi(0), vec![]);
+        assert_eq!(primepi(4), vec![0,0,1,2]);
+        assert_eq!(primepi(32), vec![0,0,1,2,2,3,3,4,4,4,4,5,5,6,6,6,6,7,7,8,8,8,8,9,9,9,9,9,9,10,10,11,11,11,11,11,11,12]);
     }
 
     #[test]
