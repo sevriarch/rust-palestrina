9e71515fbf2e129d92a456a9c27ade87af98a88b
S <sevriarch@gmail.com>
2024-12-15 13:02:44 -0500

    tests & cleanup

--- src/mutators.rs
+++ src/mutators.rs
@@ -40,6 +40,32 @@ pub fn modulus<'a, T: Copy + Num + PartialOrd + From<i8>>(
 }
 
 pub fn trim<'a, T: Num + Copy + PartialOrd>(
+    a: Option<&'a T>,
+    b: Option<&'a T>,
+) -> Result<Box<dyn Fn(T) -> T + 'a>, &'a str> {
+    match (a, b) {
+        (Some(min), Some(max)) => {
+            if min > max {
+                return Err("trim(): minimum value cannot by higher than maximum value");
+            }
+
+            Ok(Box::new(|v| {
+                if v > *max {
+                    *max
+                } else if v < *min {
+                    *min
+                } else {
+                    v
+                }
+            }))
+        }
+        (Some(min), None) => Ok(Box::new(|v| if v < *min { *min } else { v })),
+        (None, Some(max)) => Ok(Box::new(|v| if v > *max { *max } else { v })),
+        (None, None) => Ok(Box::new(|v| v)),
+    }
+}
+/*
+pub fn trim<'a, T: Num + Copy + PartialOrd>(
     a: &'a Option<T>,
     b: &'a Option<T>,
 ) -> Result<Box<dyn Fn(T) -> T + 'a>, &'a str> {
@@ -49,13 +75,14 @@ pub fn trim<'a, T: Num + Copy + PartialOrd>(
                 return Err("trim(): minimum value cannot by higher than maximum value");
             }
 
-            Ok(Box::new(|v| if v > *max { *max } else { v }))
+            Ok(Box::new(|v| if v > *max { *max } else if v < *min { *min } else { v }))
         }
         (Some(min), None) => Ok(Box::new(|v| if v < *min { *min } else { v })),
         (None, Some(max)) => Ok(Box::new(|v| if v > *max { *max } else { v })),
         (None, None) => Ok(Box::new(|v| v)),
     }
 }
+*/
 
 #[cfg(test)]
 mod tests {
@@ -95,5 +122,36 @@ mod tests {
         assert!(modulus(&0.0).is_err());
         assert_eq!(modulus(&5).unwrap()(12), 2);
         assert_f32_near!(modulus(&1.6).unwrap()(4.2), 1.0);
+    }
+
+    #[test]
+    fn test_trim() {
+        assert!(trim(Some(&10), Some(&5)).is_err());
+
+        let maxonlyi = trim(None, Some(&10)).unwrap();
+        assert_eq!(maxonlyi(5), 5);
+        assert_eq!(maxonlyi(15), 10);
+
+        let maxonlyf = trim(None, Some(&10.5)).unwrap();
+        assert_eq!(maxonlyf(5.5), 5.5);
+        assert_eq!(maxonlyf(15.5), 10.5);
+
+        let minonlyi = trim(Some(&10), None).unwrap();
+        assert_eq!(minonlyi(5), 10);
+        assert_eq!(minonlyi(15), 15);
+
+        let minonlyf = trim(Some(&10.5), None).unwrap();
+        assert_eq!(minonlyf(5.5), 10.5);
+        assert_eq!(minonlyf(15.5), 15.5);
+
+        let bothi = trim(Some(&2), Some(&10)).unwrap();
+        assert_eq!(bothi(0), 2);
+        assert_eq!(bothi(5), 5);
+        assert_eq!(bothi(15), 10);
+
+        let bothf = trim(Some(&2.5), Some(&10.5)).unwrap();
+        assert_eq!(bothf(0.5), 2.5);
+        assert_eq!(bothf(5.5), 5.5);
+        assert_eq!(bothf(15.5), 10.5);
     }
 }
