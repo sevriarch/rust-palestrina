ce1f446411e47e6a063e5faf0e11ee54c580e10c
S <sevriarch@gmail.com>
2025-01-07 11:47:47 -0500

    ops.transpose_pitch(), refactoring underneath

--- src/ops/pitch.rs
+++ src/ops/pitch.rs
@@ -6,38 +6,59 @@ use crate::sequences::{
     traits::Sequence,
 };
 
-use std::ops::{Add, Sub};
-
 trait Pitch<T> {
-    fn invert(&mut self, n: T);
+    fn transpose_pitch(&mut self, n: T);
+    fn invert_pitch(&mut self, n: T);
+}
+
+macro_rules! impl_fns_for_option {
+    ($ty:ident, for $($fn:ident)*) => ($(
+        fn $fn(&mut self, n: $ty) {
+            if let Some(p) = self.as_mut() {
+                p.$fn(n);
+            }
+        }
+    )*)
+}
+
+macro_rules! impl_fns_for_vec {
+    ($ty:ident, for $($fn:ident)*) => ($(
+        fn $fn(&mut self, n: $ty) {
+            self.iter_mut().for_each(|p| p.$fn(n));
+        }
+    )*)
+}
+
+macro_rules! impl_fns_for_melody_member {
+    ($ty:ident, for $($fn:ident)*) => ($(
+        fn $fn(&mut self, n: $ty) {
+            self.values.$fn(n)
+        }
+    )*)
 }
 
 macro_rules! impl_traits_for_raw_values {
     (for $($ty:ident)*) => ($(
         impl Pitch<$ty> for $ty {
-            fn invert(&mut self, n: $ty) {
-                *self = n + n -*self
+            fn transpose_pitch(&mut self, n: $ty) {
+                *self += n
+            }
+
+            fn invert_pitch(&mut self, n: $ty) {
+                *self = n + n - *self
             }
         }
 
         impl Pitch<$ty> for Option<$ty> {
-            fn invert(&mut self, n: $ty) {
-                if let Some(p) = self.as_mut() {
-                    p.invert(n);
-                }
-            }
+            impl_fns_for_option!($ty, for transpose_pitch invert_pitch);
         }
 
         impl Pitch<$ty> for Vec<$ty> {
-            fn invert(&mut self, n: $ty) {
-                self.iter_mut().for_each(|p| p.invert(n));
-            }
+            impl_fns_for_vec!($ty, for transpose_pitch invert_pitch);
         }
 
         impl Pitch<$ty> for MelodyMember<$ty> {
-            fn invert(&mut self, n: $ty) {
-                self.values.invert(n);
-            }
+            impl_fns_for_melody_member!($ty, for transpose_pitch invert_pitch);
         }
     )*)
 }
@@ -49,25 +70,52 @@ mod tests {
     use super::*;
 
     #[test]
-    fn invert() {
+    fn transpose_pitch() {
         let mut x = 5;
-        x.invert(6);
+        x.transpose_pitch(6);
+        assert_eq!(x, 11);
+
+        let mut x = 5.5;
+        x.transpose_pitch(6.0);
+        assert_eq!(x, 11.5);
+
+        let mut x: Option<i32> = None;
+        x.transpose_pitch(6);
+        assert_eq!(x, None);
+
+        let mut x = Some(5);
+        x.transpose_pitch(6);
+        assert_eq!(x, Some(11));
+
+        let mut x = vec![4, 5, 6];
+        x.transpose_pitch(6);
+        assert_eq!(x, vec![10, 11, 12]);
+
+        let mut x = MelodyMember::from(vec![4, 5, 6]);
+        x.transpose_pitch(6);
+        assert_eq!(x, MelodyMember::from(vec![10, 11, 12]));
+    }
+
+    #[test]
+    fn invert_pitch() {
+        let mut x = 5;
+        x.invert_pitch(6);
         assert_eq!(x, 7);
 
         let mut x = 5.5;
-        x.invert(6.0);
+        x.invert_pitch(6.0);
         assert_eq!(x, 6.5);
 
         let mut x = Some(5);
-        x.invert(6);
+        x.invert_pitch(6);
         assert_eq!(x, Some(7));
 
         let mut x = vec![4, 5, 6];
-        x.invert(6);
+        x.invert_pitch(6);
         assert_eq!(x, vec![8, 7, 6]);
 
         let mut x = MelodyMember::from(vec![4, 5, 6]);
-        x.invert(6);
+        x.invert_pitch(6);
         assert_eq!(x, MelodyMember::from(vec![8, 7, 6]));
     }
 }
