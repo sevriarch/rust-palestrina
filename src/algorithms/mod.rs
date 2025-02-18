2cc7c3cf7dd2a0b95328425666f8688a003160f3
S <sevriarch@gmail.com>
2025-02-18 10:50:42 -0500

    remove unused algorithms methods

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,6 +1,4 @@
 use num_traits::Num;
-use std::cmp::PartialOrd;
-use std::fmt::Debug;
 
 pub trait AugDim<MT> {
     fn augment_target(&self, v: &mut MT);
@@ -86,57 +84,6 @@ where
     }
 }
 
-pub fn modulus<T: Copy + Num + PartialOrd>(m: &T) -> Result<Algorithm<T>, String> {
-    if m.is_zero() {
-        return Err("modulus(): cannot modulus by zero".to_string());
-    }
-
-    Ok(Box::new(|v| {
-        let ret = *v % *m;
-
-        *v = match ret < T::zero() {
-            true => ret + *m,
-            false => ret,
-        }
-    }))
-}
-
-pub fn trim<'a, T: Num + Copy + PartialOrd + Debug>(
-    a: Option<&'a T>,
-    b: Option<&'a T>,
-) -> Result<Algorithm<'a, T>, String> {
-    match (a, b) {
-        (Some(min), Some(max)) => {
-            if min > max {
-                return Err(format!(
-                    "trim(): minimum value {:?} cannot be higher than maximum value {:?}",
-                    min, max
-                )
-                .to_string());
-            }
-
-            Ok(Box::new(|v| {
-                if *v > *max {
-                    *v = *max;
-                } else if *v < *min {
-                    *v = *min;
-                }
-            }))
-        }
-        (Some(min), None) => Ok(Box::new(|v| {
-            if *v < *min {
-                *v = *min
-            }
-        })),
-        (None, Some(max)) => Ok(Box::new(|v| {
-            if *v > *max {
-                *v = *max
-            }
-        })),
-        (None, None) => Ok(Box::new(|_| {})),
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -196,92 +143,5 @@ mod tests {
         assert_f32_near!(v, 2.625);
         diminish(&5).unwrap()(&mut v);
         assert_f32_near!(v, 0.525);
-    }
-
-    #[test]
-    fn test_modulus() {
-        assert!(modulus(&0).is_err());
-        assert!(modulus(&0.0).is_err());
-
-        let mut v = 12;
-        modulus(&5).unwrap()(&mut v);
-        assert_eq!(v, 2);
-
-        let mut v = 4.2;
-        modulus(&1.6).unwrap()(&mut v);
-        assert_f32_near!(v, 1.0);
-    }
-
-    #[test]
-    fn test_trim() {
-        assert!(trim(Some(&10), Some(&5)).is_err());
-
-        let maxonlyi = trim(None, Some(&10)).unwrap();
-
-        let mut v = 5;
-        maxonlyi(&mut v);
-        assert_eq!(v, 5);
-
-        let mut v = 15;
-        maxonlyi(&mut v);
-        assert_eq!(v, 10);
-
-        let maxonlyf = trim(None, Some(&10.5)).unwrap();
-
-        let mut v = 5.5;
-        maxonlyf(&mut v);
-        assert_eq!(v, 5.5);
-
-        let mut v = 15.5;
-        maxonlyf(&mut v);
-        assert_eq!(v, 10.5);
-
-        let minonlyi = trim(Some(&10), None).unwrap();
-
-        let mut v = 5;
-        minonlyi(&mut v);
-        assert_eq!(v, 10);
-
-        let mut v = 15;
-        minonlyi(&mut v);
-        assert_eq!(v, 15);
-
-        let minonlyf = trim(Some(&10.5), None).unwrap();
-
-        let mut v = 5.5;
-        minonlyf(&mut v);
-        assert_eq!(v, 10.5);
-
-        let mut v = 15.5;
-        minonlyf(&mut v);
-        assert_eq!(v, 15.5);
-
-        let bothi = trim(Some(&2), Some(&10)).unwrap();
-
-        let mut v = 0;
-        bothi(&mut v);
-        assert_eq!(v, 2);
-
-        let mut v = 5;
-        bothi(&mut v);
-        assert_eq!(v, 5);
-
-        let mut v = 15;
-        bothi(&mut v);
-        assert_eq!(v, 10);
-
-        let bothf = trim(Some(&2.5), Some(&10.5)).unwrap();
-
-        let mut v = 0.5;
-        bothf(&mut v);
-        assert_eq!(v, 2.5);
-
-        let mut v = 5.5;
-        bothf(&mut v);
-        assert_eq!(v, 5.5);
-
-        let mut v = 15.5;
-        bothf(&mut v);
-        assert_eq!(v, 10.5);
     }
 }
---
+++ src/midi/reader.rs
@@ -0,0 +1,6 @@
+use crate::collections::traits::Collection;
+use crate::entities::timing::Timing;
+use crate::metadata::{Metadata, MetadataData, MetadataError, MetadataList};
+use crate::score::Score;
+use crate::sequences::melody::{Melody, MelodyMember};
+use thiserror::Error;
