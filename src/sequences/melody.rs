923507577b7276fd0cd27b39a3a0edf7556be076
S <sevriarch@gmail.com>
2025-06-22 12:45:44 -0400

    Melody::with_exact_tick_at()

--- src/sequences/melody.rs
+++ src/sequences/melody.rs
@@ -465,6 +465,10 @@ where
         self.mutate_indices(ix, move |m| m.timing.duration = dur)
     }
 
+    pub fn with_exact_tick_at(self, ix: &[i32], tick: u32) -> Result<Self> {
+        self.mutate_indices(ix, move |m| m.timing.tick = Some(tick))
+    }
+
     pub fn with_event_at(self, ix: &[i32], evt: Metadata) -> Result<Self> {
         self.mutate_indices(ix, |m| {
             m.with_event(&evt.clone());
@@ -1227,6 +1231,30 @@ mod tests {
                 MelodyMember {
                     values: vec![16],
                     timing: DurationalEventTiming::new(25, None, 0),
+                    volume: DEFAULT_VOLUME,
+                    before: MetadataList::new(vec![]),
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn with_exact_tick_at() {
+        assert_eq!(
+            Melody::try_from(vec![12, 16])
+                .unwrap()
+                .with_exact_tick_at(&[-1], 25)
+                .unwrap(),
+            Melody::new(vec![
+                MelodyMember {
+                    values: vec![12],
+                    timing: DurationalEventTiming::default(),
+                    volume: DEFAULT_VOLUME,
+                    before: MetadataList::new(vec![]),
+                },
+                MelodyMember {
+                    values: vec![16],
+                    timing: DurationalEventTiming::new(0, Some(25), 0),
                     volume: DEFAULT_VOLUME,
                     before: MetadataList::new(vec![]),
                 },
